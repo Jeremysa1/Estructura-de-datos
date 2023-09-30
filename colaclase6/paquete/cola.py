b0e83ade22f72657a14e395287671533d300f35f
Jeremy Sánchez Arias <141504791+Jeremysa1@users.noreply.github.com>
2023-09-30 02:00:26 +0000

    nuevo

--- colaclase6/paquete/cola.py
+++ colaclase6/paquete/cola.py
@@ -2,15 +2,15 @@ from .tipos import Item
 
 class Nodo:
     def __init__(self, valor: Item, mensaje_id: int):
-        self.valor = valor         
-        self.mensaje_id = mensaje_id  
-        self.siguiente = None       
+        self.valor = valor
+        self.mensaje_id = mensaje_id
+        self.siguiente = None
 
 class Cola:
     def __init__(self):
-        self.primero = None           
-        self.ultimo = None            
-        self.contador_id = 1          
+        self.primero = None
+        self.ultimo = None
+        self.contador_id = 1
 
     def esta_vacia(self):
         return self.primero is None
@@ -22,63 +22,63 @@ class Cola:
         else:
             self.ultimo.siguiente = nuevo_nodo
         self.ultimo = nuevo_nodo
-        self.contador_id + = 1
-    
+        self.contador_id += 1
+
     def desencolar(self):
         if self.esta_vacia():
             return None
         valor = self.primero.valor
-        mensaje_id = self.primero.mensaje_id 
+        mensaje_id = self.primero.mensaje_id
         self.primero = self.primero.siguiente
         if self.primero is None:
             self.ultimo = None
-        return{"mensaje_id": mensaje_id, "valor": valor} #SE DEVUELVE EL MENSAJE DESENCOLADO
+        return {"mensaje_id": mensaje_id, "valor": valor}
 
-     def ver_listado(self):
-        contenido[]
-        actual = self.primero
-        while contenido:
-            contenido.append({"mensaje_id": actual.mensaje.id, "valor": self.primero.valor})
-            actual = actual.siguiente 
-            return contenido  #SE DEVUELVE LA LISTA DE MENSAJES 
+    def ver_listado(self):
+        contenido = []
+        nodo_actual = self.primero
+        while nodo_actual:
+            contenido.append({"mensaje_id": nodo_actual.mensaje_id, "valor": nodo_actual.valor})
+            nodo_actual = nodo_actual.siguiente
+        return contenido  # Devuelve la lista de mensajes
 
-     def ver ultimo(self):
+    def ver_ultimo(self):
         if self.ultimo:
             return {"mensaje_id": self.ultimo.mensaje_id, "valor": self.ultimo.valor}
         else:
             return None
-    
-     def ver_primero(self):
-         if self.primero:
+
+    def ver_primero(self):
+        if self.primero:
             return {"mensaje_id": self.primero.mensaje_id, "valor": self.primero.valor}
         else:
             return None
 
-     def contar(self):
+    def contar(self):
         count = 0
-        actual = self.primero
-        while actual is not None:
+        nodo_actual = self.primero
+        while nodo_actual:
             count += 1
-            actual = actual.siguiente  
-        return count  #.....
-        
+            nodo_actual = nodo_actual.siguiente
+        return count
 
-     def eliminar_mensaje(self, mensaje_id: int) -> bool:
-        actual = self.primero
-        anterior = None
+    def eliminar_mensaje(self, mensaje_id: int):
+        nodo_actual = self.primero
+        nodo_anterior = None
 
-        while actual and actual.mensaje_id != mensaje_id:
-            anterior = actual
-            actual = actual.siguiente
+        while nodo_actual and nodo_actual.mensaje_id != mensaje_id:
+            nodo_anterior = nodo_actual
+            nodo_actual = nodo_actual.siguiente
 
-        if actual:
-            if anterior:
-             anterior.siguiente = actual.siguiente
+        if nodo_actual:
+            if nodo_anterior:
+                nodo_anterior.siguiente = nodo_actual.siguiente
             else:
-                self.primero = actual.siguiente
+                self.primero = nodo_actual.siguiente
 
-            if not actual.siguiente:
-                self.ultimo = anterior
-            return True  # El mensaje se eliminó exitosamente
+            if not nodo_actual.siguiente:
+                self.ultimo = nodo_anterior
 
-        return False  # El mensaje con el mensaje_id no se encontró en la cola
+            return True
+
+        return False
